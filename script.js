ab0f01dbda25e4035fb3676d19221e61cf364df9
Evgenii <chico7496@gmail.com>
2023-01-27 21:58:40 +0300

    cw_1

---
+++ script.js
@@ -0,0 +1,118 @@
+// let a = 8, 
+//     b = 3;
+
+// {
+//     //a+2(x-b)=16;
+//     //2(x-b) = 16 - a
+//     // x - b = (16-a)/2
+//     //x = (16 - a) / 2 + b
+//     let x = (16 - a) / 2 + b;
+//     console.log(x);
+//     console.log(a + 2 * (x - b));
+// }
+
+// {
+//     //b(x+15)=a+6x;
+//     //bx + b15 = a + 6x;
+//     //bx + b15 - 6x = a
+//     //x(b - 6) + b15 = a
+//     //x(b - 6) = a - b15
+//     //x = (a - b15) / (b - 6)
+//     let x = (a - b * 15) / (b - 6);
+//     console.log(x);
+//     console.log(b * (x + 15) === a + 6 * x);
+// }
+
+// {
+//     //x+2x+ax+bx=23780;
+//     //x(1+2+a+b) = 23780;
+//     //x = 23780 / (3 + a + b);
+//     let x = 23780 / (3  + a + b);
+//     console.log(x);
+//     console.log(x + 2 * x + a * x + b * x);
+// }
+
+// console.log('-------------------------');
+
+// {
+//     let s = '123.3.4d2342';
+//     console.log(Number(s));
+//     console.log(+s);
+//     console.log(parseInt(s));
+//     console.log(parseFloat(s));
+// }
+
+// console.log('-------------------------');
+
+// {
+//     let n = 342;
+//     console.log(String(n));
+//     console.log(n.toString(2));
+//     console.log(n + '');
+// }
+
+// /**
+//  * Number
+//  * String
+//  * Boolean
+//  * Object
+//  * bigInt
+//  * Symbol
+//  * undefined
+//  * null
+//  */
+
+// console.log(3 && 4 && 0 && '');
+// console.log(3 && 4 && '' && 0);
+
+// console.log(undefined || null || false || 0 || '' || 2 || 5);
+
+// console.log(false ?? 5);
+// console.log('' ?? 3);
+// console.log(null ?? 4);
+// console.log(undefined ?? 7);
+
+// let x = null;
+
+// x = x ?? 0;
+
+// console.log(Math);
+// console.dir(Math);
+
+// console.warn();
+// // console.clear();
+// x = [1, 2, 3, 4]
+// console.table(x);
+// console.log(x);
+
+// console.log('Hello\nWorld');
+
+// console.log(`Hello
+// World
+// ${a + b + `Rezult: ${x}`}`);
+
+let key = 1;
+
+switch (key) {
+    case 1:
+        console.log(1);
+        // break;
+    case 2:
+        console.log(2);
+        // break;
+    default:
+        console.log('default');
+        // break;
+}
+
+// if (condition) {
+//     console.log(true);
+// }else if(v2){
+//     console.log(true + 'v2');
+// }else{
+//     console.log(false);
+// }
+
+// let c = condition ? true : false;
+
+const arr = [1, 2, 3];
