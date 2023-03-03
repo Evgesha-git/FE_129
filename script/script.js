f8de7a5bb0eb529f3df2d42c6ffc9a06eb9d2a3a
Evgenii <chico7496@gmail.com>
2023-03-03 21:33:28 +0300

    slider

--- script/script.js
+++ script/script.js
@@ -1,87 +1,49 @@
-const tabs = (selector) => {
-    const tabContainer = document.querySelectorAll(selector);
+const slider = selector => {
+    const slidersContainer = document.querySelectorAll(selector);
 
-    const contantHandler = (contents, index) => {
-        contents.forEach((content, i) => {
-            if (i === index){
-                content.classList.add('active');
-            }else{
-                content.classList.remove('active');
-            }
-        });
-    }
+    const sliderHandler = slideContainer => {
+        const sliders = slideContainer.querySelector('.slides');
+        if (!sliders) return;
 
-    const tabsHandler = (buttons, contents) => {
-        buttons.addEventListener('click', event => {
-            let button = event.target;
-            if (button.tagName !== 'LI') return;
-            [...buttons.children].forEach((btn, i) => {
-                if (btn === button){
-                    btn.classList.add('active');
-                    contantHandler([...contents.children], i);
+        const slides = slideContainer.querySelectorAll('.slide');
+        if (!slides || !slides.length > 1) return;
+
+        const buttons = slideContainer.querySelectorAll('.button');
+        if (!buttons) return;
+
+        const switchSlide = e => {
+            const buf = e.target.classList.contains('next');
+
+            let x = sliders.style.transform || '0'; // transform -> translate(-xxx%);
+            x = x.replace('translate(', "");
+            // x = x.replace(')', '');
+            x = Math.abs(parseInt(x));
+
+            if (buf){
+                if (x < (slides.length * 100) - 100){
+                    x += 100;
                 }else{
-                    btn.classList.remove('active');
+                    x = 0;
                 }
-            });
-        });
-    }
-
-    [...tabContainer].forEach(tab => {
-        const buttons = tab.querySelector('.tabs');
-        const content = tab.querySelector('.tabs__content');
-
-        tabsHandler(buttons, content);
-    });
-}
-
-tabs('.container');
-
-const tooltips = (selector) => {
-    const tooltips = document.querySelectorAll(selector);
-    let tooltipDescription = null;
-
-    const createTooltip = (event) => {
-        if (event.target.tagName !== 'SPAN') return;
-        const elem = event.target;
-        const text = elem.dataset.text;
-        console.log(elem.dataset);
-
-        let y = elem.offsetHeight + 3;
-        let x = elem.offsetWidth;
-
-        console.log(x);
-
-        tooltipDescription = document.createElement('div');
-        tooltipDescription.classList.add('tool-tip-element');
-        tooltipDescription.innerText = text;
-
-        tooltipDescription.style.top = y + 'px';
-        tooltipDescription.style.left = -x + 'px';
-
-        elem.append(tooltipDescription);
-    }
-
-    const removeTooltip = event => {
-        const elem = event.target;
-
-        [...elem.children].forEach(child => {
-            if (child.classList.contains('tool-tip-element')){
-                child.remove();
-                tooltipDescription = null;
+            }else{
+                if (x > 0){
+                    x -= 100;
+                }else{
+                    x = (slides.length * 100) - 100;
+                }
             }
-        })
+
+            sliders.style.transform = `translate(-${x}%)`;
+        }
+
+        buttons.forEach(button => {
+            button.addEventListener('click', switchSlide);
+        });
     }
 
-    tooltips.forEach(tolltip => {
-        tolltip.addEventListener('mouseover', function(event) {
-            createTooltip(event);
-        });
-        tolltip.addEventListener('mouseout', event => removeTooltip(event));
-    });
+    slidersContainer.forEach(slide => sliderHandler(slide));
 }
 
-tooltips('.tool-tip');
+slider('.main__slider');
 
-/**
- * На дом - сделать галерею фото (пример - https://learn.javascript.ru/task/image-gallery)
- */
+//Дома - написать аккордеон
