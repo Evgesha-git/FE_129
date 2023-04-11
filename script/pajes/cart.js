d21dafd5353108ec57b674475a692a6e4cb316f2
Evgenii <chico7496@gmail.com>
2023-04-11 21:14:42 +0300

    modules

--- script/pajes/cart.js
+++ script/pajes/cart.js
@@ -1,5 +1,5 @@
-class Cart{
-    constructor(){
+class Cart {
+    constructor() {
         this.widdget = document.createElement('div');
         this.widdget.classList.add('widget');
         this.cart = [];
@@ -7,21 +7,31 @@ class Cart{
         this.cartContainer.classList.add('container');
         this.init = this.init.bind(this);
         this.addCart = this.addCart.bind(this);
+        this.getStateItem = this.getStateItem.bind(this);
+        this.openRemoveItem = this.openRemoveItem.bind(this);
         this.getStorage();
     }
 
-    setStorage (){
+    setStorage() {
         localStorage.setItem('cart', JSON.stringify(this.cart));
     }
 
-    getStorage(){
+    getStorage() {
         let cart = localStorage.getItem('cart');
-        if (cart){
+        if (cart) {
             this.cart = JSON.parse(cart);
         }
     }
 
-    addCart(item){
+    getStateItem(id) {
+        if (!this.cart.length) return false;
+        let flag = this.cart.some(item => item.id === id);
+        return flag;
+    }
+
+    addCart(item) {
+        item = { count: 1, ...item };
+
         this.cart.push(item);
         this.widdget.innerHTML = `
             <a href="#cart">
@@ -32,7 +42,7 @@ class Cart{
         return true;
     }
 
-    getWdget(){
+    getWdget() {
         let conuter = this.cart.length;
 
         this.widdget.innerHTML = `
@@ -44,14 +54,55 @@ class Cart{
         return this.widdget;
     }
 
-    removeItemCart (id){
+    removeItemCart(id) {
         this.cart = this.cart.filter(item => item.id !== id);
         this.render();
         this.setStorage();
     }
 
-    render(){
-        if (!this.cart.length){
+    openRemoveItem(id) {
+        this.cart = this.cart.filter(item => item.id !== id);
+        this.getWdget();
+        this.setStorage();
+    }
+
+    changeCount(id, count = 0, flag = false) {
+        this.cart = this.cart.map(item => {
+            if (item.id === id) {
+                if (!count) {
+                    if (flag) {
+                        item.count += 1;
+                        return item;
+                    } else {
+                        if (item.count > 1) {
+                            item.count -= 1;
+                            return item;
+                        }else{
+                            return item;
+                        }
+                    }
+                } else {
+                    item.count = +count;
+                    return item;
+                }
+
+            } else {
+                return item;
+            }
+        });
+        this.render();
+        this.setStorage();
+    }
+
+    totalPrice(){
+        let price = this.cart.reduce((price, item) => price += item.price * item.count, 0);
+        let h2 = document.createElement('h2');
+        h2.innerText = `Итоговая сумма: ${price}`;
+        return h2;
+    }
+
+    render() {
+        if (!this.cart.length) {
             this.cartContainer.innerHTML = '<h2>Корзина пуста</h2>';
             return;
         }
@@ -66,18 +117,41 @@ class Cart{
                 <h3 class="item__title">${item.title}</h3>
                 <p class="price">${item.price}</p>
             `;
+            let controlContainer = document.createElement('div');
+            controlContainer.classList.add('controll');
+
+            let counter = document.createElement('div');
+            counter.classList.add('counter');
+
+            let btnMinus = document.createElement('button');
+            let btnMas = document.createElement('button');
+            let input = document.createElement('input');
+            input.setAttribute('type', 'text');
+            input.value = item.count;
+            btnMas.innerText = '+';
+            btnMinus.innerText = '-';
+
+            counter.append(btnMinus, input, btnMas);
+
+            btnMas.addEventListener('click', () => this.changeCount(item.id, 0, true));
+            btnMinus.addEventListener('click', () => this.changeCount(item.id));
+            input.addEventListener('input', () => this.changeCount(item.id, +input.value));
+
             let buttonRemove = document.createElement('button');
             buttonRemove.innerText = 'Remove';
             buttonRemove.addEventListener('click', () => {
                 this.removeItemCart(item.id)
                 this.getWdget();
             });
-            cartElem.append(buttonRemove);
+
+            controlContainer.append(counter, buttonRemove);
+            cartElem.append(controlContainer);
             this.cartContainer.append(cartElem);
         });
+        this.cartContainer.append(this.totalPrice());
     }
 
-    init(){
+    init() {
         this.render();
         return this.cartContainer;
     }
@@ -86,6 +160,8 @@ class Cart{
 let cart = new Cart();
 export const widget = cart.getWdget();
 export const addCart = cart.addCart;
+export const getStateItem = cart.getStateItem;
+export const openRemoveItem = cart.openRemoveItem;
 const init = cart.init;
 
 export default init;
