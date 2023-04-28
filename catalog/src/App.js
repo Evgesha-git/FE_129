457fff86b2cf9055cb89f256aa589469af3744f2
Evgenii <chico7496@gmail.com>
2023-04-28 21:31:51 +0300

    new catalog

---
+++ catalog/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Abaut from './pages/Abaut';
+import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Catalog from './pages/Catalog';
+import { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+
+  return (
+    <CartContext.Provider value={{cart, setCart}}>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Nav />}>
+            <Route index element={<Home />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/abaut' element={<Abaut />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    </CartContext.Provider>
+  );
+}
+
+export default App;
---
+++ catalog/src/components/Nav/index.js
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router-dom"
+import style from './Nav.module.css';
+
+const Nav = () => {
+    return (
+        <>
+        <header className={style.container}>
+            <div className={style.logo}>
+                <img src="https://via.placeholder.com/200x50" alt="#" />
+            </div>
+            <nav className={style.nav__container}>
+                <ul className={style.nav}>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/catalog'}>Catalog</Link></li>
+                    <li><Link to={'/abaut'}>Abaut</Link></li>
+                </ul>
+            </nav>
+        </header>
+        <Outlet/>
+        </>
+    )
+}
+
+export default Nav;
